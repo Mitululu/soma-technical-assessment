1fac7076df25fb986c311f89935ff095673ee075
Mitul Saha <mitul@mituls-mbp-2.mynetworksettings.com>
2025-07-16 15:08:14 -0400

    adding part2 pexels images

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,49 @@
 import { Todo } from '@prisma/client';
 import { useState, useEffect } from 'react';
 
+async function getPexelsImage(title:string, id:string) {
+  try {
+    console.log("loading image for " + title);
+    const response = await fetch(`https://api.pexels.com/v1/search?query=${title}&per_page=1`, {
+      headers: {
+        Authorization: process.env.PEXELS_KEY || "",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Pexels API error: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    const imageElement = document.getElementById(id+"-img") as HTMLImageElement;
+    const loadElement = document.getElementById(id+"-loading") as HTMLSpanElement;
+
+    if (imageElement && loadElement) {
+      imageElement.src = data.photos[0].src.small;
+      loadElement.style.display = 'none';
+    }
+    
+    return;
+  } catch (error) {
+    console.error("Error fetching image:", error);
+    return;
+  }
+}
+
 export default function Home() {
   const [newTodo, setNewTodo] = useState('');
   const [newDate, setNewDate] = useState(new Date());
+  const [depends, setDepends] = useState([]);
   const [todos, setTodos] = useState([]);
-  
   const currentDate = (new Date()).toLocaleDateString("en-us");
 
   useEffect(() => {
     fetchTodos();
   }, []);
+
+  useEffect(() => {
+    fetchTodoImages();
+  }, [todos]);
 
   const fetchTodos = async () => {
     try {
@@ -23,16 +56,25 @@ export default function Home() {
     }
   };
 
+  const fetchTodoImages = async () => {
+    try {
+      todos.map((todo:Todo) => getPexelsImage(todo.title, todo.id));
+    } catch (error) {
+      console.error('Failed to fetch todos:', error);
+    }
+  }
+
   const handleAddTodo = async () => {
     if (!newTodo.trim()) return;
     try {
       await fetch('/api/todos', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ title: newTodo, date: newDate }),
+        body: JSON.stringify({ title: newTodo, date: newDate, depends: depends }),
       });
       setNewTodo('');
       setNewDate(new Date());
+      setDepends([]);
       fetchTodos();
     } catch (error) {
       console.error('Failed to add todo:', error);
@@ -64,6 +106,11 @@ export default function Home() {
           
           />
           <input type="date" onChange={(e) => setNewDate(new Date(e.target.value))} />
+          <select multiple id="dependency-select">
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+          </select>
           <button
             onClick={handleAddTodo}
             className="bg-white text-indigo-600 p-3 rounded-r-full hover:bg-gray-100 transition duration-300"
@@ -81,6 +128,8 @@ export default function Home() {
               <span className={currentDate > (new Date(todo.date)).toLocaleDateString("en-us") ? "text-red-500" : "text-gray-800"}>
                 {todo.date ? (new Date(todo.date)).toLocaleDateString("en-us") : ""}
               </span>
+              <span id={todo.id+"-loading"} className="text-gray-800">loading</span>
+              <img id={todo.id + "-img"} />
               <button
                 onClick={() => handleDeleteTodo(todo.id)}
                 className="text-red-500 hover:text-red-700 transition duration-300"
